6609fe8b70bfc81d6c9de6c57bd1154d8343f4ff
Chris Givens <chris@solliance.net>
2018-10-08 19:24:26 -0700

    Fixes

--- Hands-on lab/WebApp/FourthCoffeeWeb/Scripts/modernizr-2.6.2.js
+++ Hands-on lab/WebApp/FourthCoffeeWeb/Scripts/modernizr-2.6.2.js
@@ -625,10 +625,6 @@ window.Modernizr = (function( window, document, undefined ) {
         return (/^0.55$/).test(mStyle.opacity);
     };
 
-
-    // Note, Android < 4 will pass this test, but can only animate
-    //   a single property at a time
-    //   daneden.me/2011/12/putting-up-with-androids-bullshit/
     tests['cssanimations'] = function() {
         return testPropsAll('animationName');
     };
